73b75ed7e01052f93ea29c22eadbf1dbcae2ddb0
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-10-17 13:25:43 +0100

    What's wrong with this lab?

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,21 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  #validates :release_year, numericality: { less_than: 2019 }
+  #validates :release_year, presence: true, if: :released
+  validates :title, uniqueness: { scope: [:title, :release_year] }
+  with_options if: :released? do |song|
+      song.validates :release_year, presence: true
+      song.validates :release_year, numericality: { less_than: 2019 }
+    end
+
+  #def initiliaze
+  #    if self.release_year
+  #      self.released = true
+  #    end
+  #end
+
+
+  def released?
+    released == true
+  end
 end
